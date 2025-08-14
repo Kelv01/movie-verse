ed32f5fca9fb559d359e3c24a0fd43b1172b42f5
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-14 11:28:03 +0300

    created and designed Navbar, and footer

---
+++ movie-verse/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import Header from "./components/layout/Header";
+import HomePage from "./components/HomePage";
+import Footer from "./components/layout/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/Now-Playing" element={<Nowplaying />} />
+          <Route path="/Upcoming Movies" element={<UpcomingMovies />} />
+          <Route path="/Genres" element={<Genres />} />
+          <Route path="/Reviews" element={<Reviews />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
