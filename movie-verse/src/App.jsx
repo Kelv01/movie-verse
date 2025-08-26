5f041468120e7cc83724ec8e8ee3626be30126d8
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-26 20:10:42 +0300

    added fixed footer with layout component

--- movie-verse/src/App.jsx
+++ movie-verse/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
+import Popular from "./pages/Popular";
+import Upcoming from "./pages/Upcoming";
+import Genres from "./pages/Genres";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          
+          <Route path="/" element={<Layout />} >
+          <Route index element={<HomePage />} />
+          <Route path="popular" element={<Popular />} />
+          <Route path="upcoming" element={<Upcoming />} />
+          <Route path="genres" element={<Genres />} />
+          </Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   );
